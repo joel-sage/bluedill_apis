058a99ffba2fcdc2ae9ab15b463dffd6ffd19122
Sage <ezeonyedikachi41@gmail.com>
2023-08-01 11:09:01 +0100

    modified the data returned from login endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,13 +42,13 @@ const create = async (req, res) => {
 // User Login Authentication
 const loginWithAuthentication = async (req, res) => {
   const { password , email } = req.body;
-  db_con.query(`SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`,
+  db_con.query(`SELECT id, first_name, last_name, user_id, email, company FROM users WHERE email = '${email}' AND password = '${password}'`,
     (err, userPayload) => {
       if (err) throw err;
       if (Object.keys(userPayload).length > 0) {
         const token = jwt.sign({ userPayload }, process.env.NODE_JWT_SIGN_TOKEN_KEY, { expiresIn: "1 day" });
         res.cookie("token", token, {httpOnly:false})
-        res.status(200).json({"message": "User Authenticated", userPayload})
+        res.status(200).json({ tokenHash: token, "Message": "User Authenticated", payLoad: userPayload})
       } else {
         res.status(400).json({ message: "Invalid Email or Password" });
       }
